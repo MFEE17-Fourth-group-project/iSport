c04ea6f92e56880ae1eb7e96db9023e4907f10a8
TaylorLiu11 <liuayo1111@gmail.com>
2021-08-22 23:55:04 +0800

    泰勒增加影相關片資料表及避免.vscode/被上傳

---
+++ mySql/video_tag.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： localhost
+-- 產生時間： 2021 年 08 月 22 日 17:49
+-- 伺服器版本： 10.4.19-MariaDB
+-- PHP 版本： 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `mfee17_4_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `video_tag`
+--
+
+CREATE TABLE `video_tag` (
+  `video_id` int(6) UNSIGNED NOT NULL,
+  `tag_id` int(6) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
