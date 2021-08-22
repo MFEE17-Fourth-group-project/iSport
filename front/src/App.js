475ce668a543c3cb8bdc7b8c8933b416c5c302fa
TaylorLiu11 <liuayo1111@gmail.com>
2021-08-23 04:13:36 +0800

    初始版設定完成，增加tailwind

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+    return (
+        <div className="App">
+            <div className="text-xl bg-green-400">
+                Hello
+            </div>
+            <div className="">
+                Hello
+            </div>
+        </div>
+    );
+}
+
+export default App;
