b126e0a8e1d51cba4c1b27a401a407e4f8a326ff
Sarah <hsue1207@gmail.com>
2021-08-23 21:08:59 +0800

    測試Router - 補推

--- front/src/index.js
+++ front/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/main.css';
 import App from './App';
+import RouterPages from './RouterPages';
 
 ReactDOM.render(
     <React.StrictMode>
         <App />
+        <RouterPages />
     </React.StrictMode>,
     document.getElementById('root')
 );
---
+++ front/src/pages/user/articles/Articles.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Articles(props) {
+
+  return (
+    <>
+    <h1>Articles</h1>
+    </>
+  )
+}
+
+export default Articles
