d0ac8d7a53d71f707b0d2f2fbe7e0094cacd0646
TaylorLiu11 <liuayo1111@gmail.com>
2021-08-23 10:58:24 +0800

    製作一些共用元件

--- front/src/App.js
+++ front/src/App.js
@@ -7,6 +7,21 @@ function App() {
             <div className="">
                 Hello
             </div>
+            <button className="btn-blue">按鈕</button>
+            <button className="btn-green">按鈕</button>
+            <button className="btn-yellow">按鈕</button>
+            <button className="btn-gray">按鈕</button>
+            <button className="btn-red">按鈕</button>
+
+            <div class="user-image-sm">
+                <img src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg"
+                    alt="..." class="image-round" />
+            </div>
+
+            <div class="user-image-bg">
+                <img src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg"
+                    alt="..." class="image-round" />
+            </div>
         </div>
     );
 }
