b1ee3143642673770de33c522799af624148098b
Sarah <hsue1207@gmail.com>
2021-08-23 21:01:58 +0800

    測試Router

---
+++ front/src/RouterPages.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+
+import Articles from './pages/articles/Articles'
+import Products from './pages/products/Product'
+import User from './pages/user/User'
+import Video from './pages/videos/Video'
+
+function RouterPages() {
+
+  return (
+    <Router>
+      <>
+        <Link to="/articles">Articles</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/user">User</Link>
+        <Link to="/video">Video</Link>
+
+        <Switch>
+          <Route path="/articles">
+            <Articles />
+          </Route>
+          <Route path="/products">
+            <Products />
+          </Route>
+          <Route path="/user">
+            <User />
+          </Route>
+          <Route path="/video">
+            <Video />
+          </Route>
+        </Switch>
+      </>
+    </Router>
+  )
+}
+
+export default RouterPages
