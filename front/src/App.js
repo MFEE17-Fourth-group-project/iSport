d3207ed827378fd429e19c1634f3a39c63d8d5fb
TaylorLiu11 <liuayo1111@gmail.com>
2021-08-23 18:35:02 +0800

    Create navbar

--- front/src/App.js
+++ front/src/App.js
@@ -1,31 +1,23 @@
+import Logo from './images/biceps.svg';
+
 function App() {
     return (
         <div className="App">
-            <div className="text-xl bg-green-400">
-                Hello
-            </div>
-            <div className="">
-                Hello
-            </div>
-            <button className="btn-blue">按鈕</button>
-            <button className="btn-green">按鈕</button>
-            <button className="btn-yellow">按鈕</button>
-            <button className="btn-gray">按鈕</button>
-            <button className="btn-red">按鈕</button>
+            <div className="bg-gray-900 px-4 py-1.5 flex justify-between items-center sticky">
 
-            <div class="user-image-sm">
-                <img src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg"
-                    alt="..." class="image-round" />
-            </div>
-            <div class="user-image-sm">
-                <img src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg"
-                    alt="..." class="image-round" />
-            </div>
+                <div className="flex items-center">
+                    <img src={Logo} width="48" alt="Logo" className="mr-2" />
+                </div>
 
+                <div className="flex items-center ">
+                    <button className="btn-yellow mx-4">登入</button>
+                    <a href="#" className="text-white text-opacity-70 mx-4">精選影片</a>
+                    <a href="#" className="text-white text-opacity-70 mx-4">多樣商品</a>
+                    <a href="#" className="text-white text-opacity-70 mx-4">優質文章</a>
+                    <a href="#" className="text-white text-opacity-70 mx-4">健身房</a>
+                    <a href="#" className="text-white text-opacity-70 mx-4">購物車</a>
+                </div>
 
-            <div class="user-image-bg">
-                <img src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg"
-                    alt="..." class="image-round" />
             </div>
         </div>
     );
