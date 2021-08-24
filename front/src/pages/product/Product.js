98c1e68c1fc2eaa78afd257d49b22887a7eb46e0
Reverie <83707139+h221391ba@users.noreply.github.com>
2021-08-24 22:15:18 +0800

    feature-html切版-product header(暫時)

--- front/src/pages/product/Product.js
+++ front/src/pages/product/Product.js
@@ -1,9 +1,26 @@
 import React from "react";
+import productHeader from "../../images/product-header.png";
 
 export default function Product() {
   return (
     <>
-      <header>header</header>
+      <header className="relative w-full h-4/5 overflow-hidden">
+        <div className="text-white absolute">
+          <p className="text-h5">
+            Practice Hard, Play Strong, Pain Is Glory. ( Only For Women )
+          </p>
+          <h1 className="text-h1">
+            Solutions For Moving Better And Feeling Althier.
+          </h1>
+        </div>
+        <figure className="h-full w-full ">
+          <img
+            src={productHeader}
+            alt="productHeader"
+            className="w-full h-full object-cover object-center"
+          />
+        </figure>
+      </header>
       <nav className="bg-gray-900 flex justify-center">
         <div className="border-b-2 border-yellow-400 w-36 text-white text-center text-base py-3">
           所有商品
