263943de325b3156c4e7f740e78c3c40ed932453
Reverie <83707139+h221391ba@users.noreply.github.com>
2021-08-24 20:45:39 +0800

    add file filter.js

--- front/src/pages/product/Product.js
+++ front/src/pages/product/Product.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Product() {
-  return <div>123456</div>;
+  return (
+    <main className="bg-gray-800">
+      <h1>12345645316</h1>
+    </main>
+  );
 }
