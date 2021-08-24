0191fd6132626ccb97a8490bfe211fb8ee92fed7
Reverie <83707139+h221391ba@users.noreply.github.com>
2021-08-24 20:58:26 +0800

    上被景色(暫時之後再改)

---
+++ front/src/Appp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeRouter from "./pages/HomeRouter";
+
+function Appp() {
+  return (
+    <div className="h-screen bg-gray-800">
+      <HomeRouter />
+    </div>
+  );
+}
+
+export default Appp;
