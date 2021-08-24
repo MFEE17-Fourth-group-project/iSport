a43cea4b2119414e04fda7fc6167d2dd1f18710e
Reverie <83707139+h221391ba@users.noreply.github.com>
2021-08-24 21:44:09 +0800

    feature-html切版-小nav

--- front/src/pages/product/Product.js
+++ front/src/pages/product/Product.js
@@ -2,8 +2,30 @@ import React from "react";
 
 export default function Product() {
   return (
-    <main className="bg-gray-800">
-      <h1>12345645316</h1>
-    </main>
+    <>
+      <header>header</header>
+      <nav className="bg-gray-900 flex justify-center">
+        <div className="border-b-2 border-yellow-400 w-36 text-white text-center text-base py-3">
+          所有商品
+        </div>
+        <div className="border-b-2 border-transparent w-36 text-white text-center text-base py-3">
+          運動服飾
+        </div>
+        <div className="border-b-2 border-transparent w-36 text-white text-center text-base py-3">
+          運動鞋類
+        </div>
+        <div className="border-b-2 border-transparent w-36 text-white text-center text-base py-3">
+          健身器材
+        </div>
+        <div className="border-b-2 border-transparent w-36 text-white text-center text-base py-3">
+          營養補給
+        </div>
+      </nav>
+      <main className="max-w-screen-xl my-0 mx-auto">
+        main
+        <section>filter</section>
+        <section>products</section>
+      </main>
+    </>
   );
 }
