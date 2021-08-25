d7f464857cb0659ebb5bfc585010347563a91fb5
Reverie <83707139+h221391ba@users.noreply.github.com>
2021-08-26 05:22:17 +0800

    feature-商品主頁初稿

--- front/src/Appp.js
+++ front/src/Appp.js
@@ -1,12 +1,12 @@
-import React from "react";
-import HomeRouter from "./pages/HomeRouter";
+import React from 'react';
+import HomeRouter from './pages/HomeRouter';
 
 function Appp() {
-  return (
-    <div className="h-full bg-gray-800">
-      <HomeRouter />
-    </div>
-  );
+    return (
+        <div className="h-full bg-gray-800">
+            <HomeRouter />
+        </div>
+    );
 }
 
 export default Appp;
--- front/src/pages/HomeRouter.js
+++ front/src/pages/HomeRouter.js
@@ -1,40 +1,40 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
-import Articles from "./article/Article";
-import Products from "./product/Product";
-import User from "./user/User";
-import Video from "./video/Video";
-import Nav from "../global/Nav";
-import Footer from "../global/Footer";
+import Articles from './article/Article';
+import Products from './product/Product';
+import User from './user/User';
+import Video from './video/Video';
+import Nav from '../global/Nav';
+import Footer from '../global/Footer';
 
 function HomeRouter() {
-  return (
-    <Router>
-      <>
-        {/* <Link to="/articles">Articles</Link>
+    return (
+        <Router>
+            <>
+                {/* <Link to="/articles">Articles</Link>
         <Link to="/products">Products</Link>
         <Link to="/user">User</Link>
         <Link to="/video">Video</Link> */}
-        <Nav />
-        <Switch>
-          <Route path="/articles">
-            <Articles />
-          </Route>
-          <Route path="/products">
-            <Products />
-          </Route>
-          <Route path="/user">
-            <User />
-          </Route>
-          <Route path="/video">
-            <Video />
-          </Route>
-        </Switch>
-        <Footer />
-      </>
-    </Router>
-  );
+                <Nav />
+                <Switch>
+                    <Route path="/articles">
+                        <Articles />
+                    </Route>
+                    <Route path="/products">
+                        <Products />
+                    </Route>
+                    <Route path="/user">
+                        <User />
+                    </Route>
+                    <Route path="/video">
+                        <Video />
+                    </Route>
+                </Switch>
+                <Footer />
+            </>
+        </Router>
+    );
 }
 
 export default HomeRouter;
