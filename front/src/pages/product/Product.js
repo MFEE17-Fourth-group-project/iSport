45b815ae66a60f310e39a71802eddffc8ce4f08d
Reverie <83707139+h221391ba@users.noreply.github.com>
2021-08-26 03:11:20 +0800

    setting-暫時將縮排改成4並關閉 漂亮 警告

--- front/src/pages/product/Product.js
+++ front/src/pages/product/Product.js
@@ -2,87 +2,90 @@ import React from 'react';
 import productHeader from '../../images/product-header.png';
 
 export default function Product() {
-  return (
-    <>
-      <header className="relative w-full h-4/5 overflow-hidden">
-        <div className="text-white absolute w-1/2 top-1/2 left-32 transform -translate-y-1/2">
-          <p className="text-2xl break-all w-full leading-normal">
-            Practice Hard, Play Strong, Pain Is Glory. ( Only For Women )
-          </p>
-          <h1 className="text-6xl break-all w-full leading-normal">
-            Solutions For Moving Better And Feeling Althier.
-          </h1>
-        </div>
-        <figure className="h-full w-full ">
-          <img
-            src={productHeader}
-            alt="productHeader"
-            className="w-full h-full object-cover object-center"
-          />
-        </figure>
-      </header>
-      <nav className="bg-gray-900 flex justify-center">
-        <div className="border-b-2 border-yellow-400 w-36 text-white text-center text-base py-3">
-          所有商品
-        </div>
-        <div className="border-b-2 border-transparent w-36 text-white text-center text-base py-3">
-          運動服飾
-        </div>
-        <div className="border-b-2 border-transparent w-36 text-white text-center text-base py-3">
-          運動鞋類
-        </div>
-        <div className="border-b-2 border-transparent w-36 text-white text-center text-base py-3">
-          健身器材
-        </div>
-        <div className="border-b-2 border-transparent w-36 text-white text-center text-base py-3">
-          營養補給
-        </div>
-      </nav>
-      <main className="max-w-screen-xl my-0 mx-auto">
-        main
-        {/* filter按鈕 */}
-        <section>
-          <div className="flex justify-between">
-            <div className="flex items-center">
-              <i class="fas fa-filter text-white text-2xl cursor-pointer"></i>
-              <button className="btn-yellow-sm mx-4">
-                價格<i class="fas fa-angle-up ml-1"></i>
-              </button>
-              <button className="btn-gray-sm mx-4">最新</button>
-              <button className="btn-yellow-sm mx-4">最熱銷</button>
-            </div>
-            <div className="w-1/3 bg-gray-700 rounded-full flex items-center px-4">
-              <input
-                className="bg-transparent outline-none border-none flex-grow placeholder-gray-700::placeholder text-white "
-                placeholder="關鍵字..."
-              />
-              <i class="fas fa-search text-white text-xl cursor-pointer"></i>
-            </div>
-          </div>
+    return (
+        <>
+            <header className="relative w-full h-4/5 overflow-hidden">
+                <div className="text-white absolute w-1/2 top-1/2 left-32 transform -translate-y-1/2">
+                    <p className="text-2xl break-all w-full leading-normal">
+                        Practice Hard, Play Strong, Pain Is Glory. ( Only For
+                        Women )
+                    </p>
+                    <h1 className="text-6xl break-all w-full leading-normal">
+                        Solutions For Moving Better And Feeling Althier.
+                    </h1>
+                </div>
+                <figure className="h-full w-full ">
+                    <img
+                        src={productHeader}
+                        alt="productHeader"
+                        className="w-full h-full object-cover object-center"
+                    />
+                </figure>
+            </header>
+            <nav className="bg-gray-900 flex justify-center">
+                <div className="border-b-2 border-yellow-400 w-36 text-white text-center text-base py-3">
+                    所有商品
+                </div>
+                <div className="border-b-2 border-transparent w-36 text-white text-center text-base py-3">
+                    運動服飾
+                </div>
+                <div className="border-b-2 border-transparent w-36 text-white text-center text-base py-3">
+                    運動鞋類
+                </div>
+                <div className="border-b-2 border-transparent w-36 text-white text-center text-base py-3">
+                    健身器材
+                </div>
+                <div className="border-b-2 border-transparent w-36 text-white text-center text-base py-3">
+                    營養補給
+                </div>
+            </nav>
+            <main className="max-w-screen-xl my-0 mx-auto">
+                main
+                {/* filter按鈕 */}
+                <section>
+                    <div className="flex justify-between">
+                        <div className="flex items-center">
+                            <i class="fas fa-filter text-white text-2xl cursor-pointer"></i>
+                            <button className="btn-yellow-sm mx-4">
+                                價格<i class="fas fa-angle-up ml-1"></i>
+                            </button>
+                            <button className="btn-gray-sm mx-4">最新</button>
+                            <button className="btn-yellow-sm mx-4">
+                                最熱銷
+                            </button>
+                        </div>
+                        <div className="w-1/3 bg-gray-700 rounded-full flex items-center px-4">
+                            <input
+                                className="bg-transparent outline-none border-none flex-grow placeholder-gray-700::placeholder text-white "
+                                placeholder="關鍵字..."
+                            />
+                            <i class="fas fa-search text-white text-xl cursor-pointer"></i>
+                        </div>
+                    </div>
 
-          <div className="bg-gray-900 p-6 flex rounded-lg">
-            <div>
-              <p className="border-l-2 border-yellow-400 text-white pl-3 mb-2.5">
-                價格區間
-              </p>
-              <div className="w-72 py-1 bg-gray-700 rounded-full flex justify-between items-center px-4">
-                <input
-                  className="w-24 bg-transparent outline-none border-none placeholder-gray-700::placeholder text-white text-center "
-                  placeholder="最小值"
-                />
-                <i class="fas fa-minus mx-1 text-yellow-400"></i>
-                <input
-                  className="w-24 bg-transparent outline-none border-none placeholder-gray-700::placeholder text-white text-center"
-                  placeholder="最大值"
-                />
-              </div>
-            </div>
+                    <div className="bg-gray-900 p-6 flex rounded-lg">
+                        <div>
+                            <p className="border-l-2 border-yellow-400 text-white pl-3 mb-2.5">
+                                價格區間
+                            </p>
+                            <div className="w-72 py-1 bg-gray-700 rounded-full flex justify-between items-center px-4">
+                                <input
+                                    className="w-24 bg-transparent outline-none border-none placeholder-gray-700::placeholder text-white text-center "
+                                    placeholder="最小值"
+                                />
+                                <i class="fas fa-minus mx-1 text-yellow-400"></i>
+                                <input
+                                    className="w-24 bg-transparent outline-none border-none placeholder-gray-700::placeholder text-white text-center"
+                                    placeholder="最大值"
+                                />
+                            </div>
+                        </div>
 
-            <div></div>
-          </div>
-        </section>
-        <section>products</section>
-      </main>
-    </>
-  );
+                        <div></div>
+                    </div>
+                </section>
+                <section>products</section>
+            </main>
+        </>
+    );
 }
