a469de6a292ab80a371d29268ca586904e21e152
TaylorLiu11 <liuayo1111@gmail.com>
2021-08-26 21:21:59 +0800

    調整App.js檔案及新增Video.js檔案、修改Video的Router

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,12 @@
-import Logo from './images/biceps.svg';
+import React from "react";
+import HomeRouter from "./pages/HomeRouter";
 
-function App() {
+function Appp() {
     return (
-        <div className="App">
-            <div className="bg-gray-900 px-4 py-1.5 flex justify-between items-center sticky">
-
-                <div className="flex items-center">
-                    <img src={Logo} width="48" alt="Logo" className="mr-2" />
-                </div>
-
-                <div className="flex items-center ">
-                    <button className="btn-yellow mx-4">登入</button>
-                    <a href="#" className="text-white text-opacity-70 mx-4">精選影片</a>
-                    <a href="#" className="text-white text-opacity-70 mx-4">多樣商品</a>
-                    <a href="#" className="text-white text-opacity-70 mx-4">優質文章</a>
-                    <a href="#" className="text-white text-opacity-70 mx-4">健身房</a>
-                    <a href="#" className="text-white text-opacity-70 mx-4">購物車</a>
-                </div>
-
-            </div>
+        <div>
+            <HomeRouter />
         </div>
     );
 }
 
-export default App;
+export default Appp;
