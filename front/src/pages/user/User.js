7b3df9950b3b5c0af77b220c62db1f83ce3dab95
cliff77777 <s19910507@gmail.com>
2021-08-27 20:19:38 +0800

    新增加user樣板user基本資料-submenu

--- front/src/pages/user/User.js
+++ front/src/pages/user/User.js
@@ -7,26 +7,38 @@ function Users() {
     return (
         <main className="max-w-screen-xl mx-auto px-2.5 py-5 flex justify-start border-red-300">
         <aside className="w-64 mr-2.5 bg-gray-900 rounded-t-xl shadow-xl">
-            <div className=" w-64 h-64">
-            <figure className="w-48 h-48 rounded-full bg-white m-auto overflow-hidden">
+            <div className=" w-64 h-64 flex justify-center items-center">
+            <div className="w-48 h-48 rounded-full bg-white overflow-hidden">
             <img
                         src={userHeader}
                         alt=""
                         className="w-full h-full object-cover object-center"
                     />
-            </figure>
             </div>
-            <div className="h-screen">
-                <ul className="text-white text-lg aside-menu">
+            </div>
+            <div className="h-full aside-menu">
+                <ul className="text-white text-lg ">
                     <li className="users-li"><a href=""> 會員資料</a></li>
-                    <li  className="users-li"><a href="">訂單管理</a></li>
+                    <li  className="users-li"><a href="">訂單管理</a>
                         <ul className="submenu">
-                                <li className="users-li"><a href="">購買紀錄</a></li>
-                                <li className="users-li"><a href="">購物車</a></li>
-                                <li className="users-li"><a href="">我的最愛</a></li>
+                                <li><a href="">購買紀錄</a></li>
+                                <li><a href="">購物車</a></li>
+                                <li><a href="">我的最愛</a></li>
                         </ul>
-                    <li  className="users-li"><a href="">影片收藏</a></li>
-                    <li  className="users-li"><a href="">文章管理</a></li>
+                        </li>
+                    <li  className="users-li"><a href="">影片收藏</a>
+                        <ul className="submenu">
+                            <li><a href="">我的最愛</a></li>
+                            <li><a href="">稍後觀看</a></li>
+                        </ul>
+                    </li>
+                    <li  className="users-li"><a href="">文章管理</a>
+                        <ul className="submenu"> 
+                                <li><a href="">我的文章</a></li>
+                                <li><a href="">新增文章</a></li>
+                                <li><a href="">收藏文章</a></li>
+                        </ul>
+                    </li>
                     <li  className="users-li"><a href="">聯絡我們</a></li>
                 </ul>
             </div>
