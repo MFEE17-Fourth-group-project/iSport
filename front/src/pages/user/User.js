9548c5d453964cb8c56ea6ff9dbf0f4a518689a6
cliff77777 <s19910507@gmail.com>
2021-08-27 23:41:40 +0800

    user樣板大致完善 taiwindcss 增加

--- front/src/pages/user/User.js
+++ front/src/pages/user/User.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import userHeader from '../../images/user/pic04.jpg';
-// import {FontAwesomeIcon} from `@fortawesome/react-fontawesome`;
+import {FaUserAlt} from 'react-icons/fa';
+import {FaShoppingCart} from 'react-icons/fa';
+import {FaHeart} from 'react-icons/fa';
+import {FaMoneyCheck} from 'react-icons/fa';
+import {FaComment} from 'react-icons/fa';
+
 
 
 function Users() {
@@ -18,28 +23,28 @@ function Users() {
             </div>
             <div className="h-full aside-menu">
                 <ul className="text-white text-lg ">
-                    <li className="users-li"><a href=""> 會員資料</a></li>
-                    <li  className="users-li"><a href="">訂單管理</a>
+                <li className="users-li"><a href=""><FaUserAlt className="userIcons"/> 會員資料</a></li>
+                    <li  className="users-li"><a href=""><FaShoppingCart className="userIcons"/>訂單管理</a>
                         <ul className="submenu">
                                 <li><a href="">購買紀錄</a></li>
                                 <li><a href="">購物車</a></li>
                                 <li><a href="">我的最愛</a></li>
                         </ul>
                         </li>
-                    <li  className="users-li"><a href="">影片收藏</a>
+                    <li  className="users-li"><a href=""><FaHeart className="userIcons"/>影片收藏</a>
                         <ul className="submenu">
                             <li><a href="">我的最愛</a></li>
                             <li><a href="">稍後觀看</a></li>
                         </ul>
                     </li>
-                    <li  className="users-li"><a href="">文章管理</a>
+                    <li  className="users-li"><a href=""><FaMoneyCheck className="userIcons"/>文章管理</a>
                         <ul className="submenu"> 
                                 <li><a href="">我的文章</a></li>
                                 <li><a href="">新增文章</a></li>
                                 <li><a href="">收藏文章</a></li>
                         </ul>
                     </li>
-                    <li  className="users-li"><a href="">聯絡我們</a></li>
+                    <li  className="users-li"><a href=""><FaComment className="userIcons"/>聯絡我們</a></li>
                 </ul>
             </div>
         </aside>
