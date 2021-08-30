d6612a855d9e840217bbbbc4fa383ec46578aa18
TaylorLiu11 <liuayo1111@gmail.com>
2021-08-31 03:18:54 +0800

    分割首頁元件

---
+++ front/src/pages/homeComponents/BannerSection.js
@@ -0,0 +1,30 @@
+import HomePage1 from './../../images/homepage/homepage-1.jpg';
+import HomePage2 from './../../images/homepage/homepage-2.jpg';
+import HomePage3 from './../../images/homepage/homepage-3.jpg';
+import HomePage4 from './../../images/homepage/homepage-4.jpg';
+import { BsFillCaretRightFill } from 'react-icons/bs';
+import { BsFillCaretLeftFill } from 'react-icons/bs';
+
+const BannerSection = () => {
+    return (
+        <div className="relative h-full w-full">
+            <BsFillCaretLeftFill className="text-yellow-400 w-16 h-16 absolute inset-y-1/2 transform -translate-y-1/2 cursor-pointer" />
+            <img
+                src={HomePage1}
+                className="h-full w-full object-cover"
+                alt=""
+            />
+            <BsFillCaretRightFill className="text-yellow-400 w-16 h-16 absolute inset-y-1/2 right-0 transform -translate-y-1/2 cursor-pointer" />
+            <div className="absolute bottom-40 left-40">
+                <h1 className="font-normal text-5xl text-white tracking-wide leading-snug mb-4">
+                    提供健身資訊分享平台與眾多
+                        <br />
+                        專業健身影片、文章分享、商城。
+                    </h1>
+                <button className="btn-yellow">立即加入</button>
+            </div>
+        </div>
+    );
+};
+
+export default BannerSection;
---
+++ front/src/pages/homeComponents/VideoSection.js
@@ -0,0 +1,35 @@
+import HomePage8 from './../../images/homepage/homepage-8.jpg';
+import VideoCard from './../video/components/VideoCard';
+import VideoCard2 from './../video/components/VideoCard2';
+
+const VideoSection = () => {
+    return (
+        <div className="w-full h-full flex">
+            <div className="w-2/3">
+                <div className="mt-6 ml-24">
+                    <h2 className="text-white text-4xl">精選影片</h2>
+                    <p className="text-white text-2xl border-l-4 border-yellow-400 pl-7 py-6 ml-10 my-7">
+                        各式各樣的健身相關影片，讓您無論在戶外或是家裡都可以隨時
+                            <br />
+                            隨地觀看，不需擔心固定時間與定點的課程。
+                        </p>
+                </div>
+                <div className="flex justify-between">
+                    <VideoCard />
+                    <VideoCard2 />
+                    <VideoCard />
+                </div>
+                <div className=""></div>
+            </div>
+            <div className="w-1/3 ml-8 relative">
+                <img
+                    className="w-full rounded-3xl object-cover"
+                    src={HomePage8}
+                />
+                {/* <BsCursorFill /> */}
+            </div>
+        </div>
+    );
+};
+
+export default VideoSection;
