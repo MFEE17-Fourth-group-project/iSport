8700be56a598887818e196add31a25eaa79f4630
TaylorLiu11 <liuayo1111@gmail.com>
2021-09-02 02:14:53 +0800

    新增影片主頁RWD

--- front/src/pages/video/components/VideoCard2.js
+++ front/src/pages/video/components/VideoCard2.js
@@ -6,12 +6,12 @@ import { Link } from 'react-router-dom';
 
 const VideoCard = () => {
     return (
-        <Link to="/video/:videoId">
-            <div className="w-75 rounded-md overflow-hidden shadow-xl bg-gray-900 py-4 px-5 cursor-pointer">
-
-                <img className="w-full h-44 object-fit" src={cardImg2} alt="" />
+        <Link to="/video/:videoId" className="max-w-max m-auto">
+            <div className="max-w-xs rounded-md overflow-hidden shadow-2xl bg-gray-900
+                py-4 px-5 flex flex-col">
+                <img className="max-h-44 object-cover" src={cardImg2} alt="" />
                 <div className="">
-                    <h3 className="text-base text-yellow-400 my-6">15 分鐘高强度全身肌肉 無需器材又能在家做的運動</h3>
+                    <h3 className="text-base text-yellow-400 my-4">15 分鐘高强度全身肌肉 無需器材又能在家做的運動</h3>
                     <p className="text-white text-sm text-opacity-70 mb-6">
                         分享給大家 - 【15分鐘的全身徒手訓練】
                         即使被禁在家，依然不阻礙我們想訓練的心！
