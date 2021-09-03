eef3b6dce325eddc4b7f16c2ca4570e8d8c342d5
TaylorLiu11 <liuayo1111@gmail.com>
2021-09-03 12:27:12 +0800

    調整健身房、文章的標題 RWD

--- front/src/pages/homeComponents/ArticleSection.js
+++ front/src/pages/homeComponents/ArticleSection.js
@@ -9,16 +9,16 @@ const ArticleSection = () => {
             <BsFillCaretLeftFill className="text-yellow-400 w-16 h-16 absolute inset-y-1/2 transform -translate-y-1/2 cursor-pointer z-10" />
 
             <BsFillCaretRightFill className="text-yellow-400 w-16 h-16 absolute inset-y-1/2 right-0 transform -translate-y-1/2 cursor-pointer" />
-            <div className="text-center py-6 my-7">
-                <span className="text-white text-4xl border-b-2 border-yellow-400 py-6 my-7">
+            <div className="text-center py-6 mt-3.5 sm:my-7">
+                <span className="text-white text-2xl lg:text-3xl xl:text-4xl border-b-2 sm:border-b-4 border-yellow-400 pb-2.5 sm:pb-3.5">
                     精選文章
                 </span>
             </div>
-            <p className="text-white text-2xl text-center w-full">
+            <p className="text-white text-xl sm:text-2xl text-center w-full px-4">
                 這裡有豐富強大的健身資料庫，提供您健身資訊、營養菜單...
             </p>
             <div className="object-cover filter brightness-100 h-112">
-                <div className="mx-auto my-20 h-full overflow-y-auto w-3/4">
+                <div className="mx-auto my-8 sm:my-20 h-full overflow-y-auto w-3/4">
                     <Link className="flex-1" to="/articles">
                         <Article />
                     </Link>
