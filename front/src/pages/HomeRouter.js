1ffb4d893e27728971bf9ad3b1957913f2565dbd
Reverie <83707139+h221391ba@users.noreply.github.com>
2021-09-05 00:42:02 +0800

    fix-change file path:ProductItem.js,and router

--- front/src/pages/HomeRouter.js
+++ front/src/pages/HomeRouter.js
@@ -10,7 +10,7 @@ import Checkout2 from './user/cart/Checkout2';
 import Checkout from './user/cart/Checkout';
 import Nav from '../global/Nav';
 import Footer from '../global/Footer';
-import ProductItem from './product/ProductItem';
+import ProductItem from './product/components/ProductItem'; //改路徑
 import VideoId from './video/VideoId';
 import FavouriteProduct from './user/cart/FavouriteProduct.js';
 import ArticleCategory from './article/ArticleCategory';
