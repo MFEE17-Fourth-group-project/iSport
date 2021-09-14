e63c86824b220cc715a9102019d00c9559901b29
TaylorLiu11 <liuayo1111@gmail.com>
2021-09-14 12:22:18 +0800

    Create video routes

--- back/routers/videos.js
+++ back/routers/videos.js
@@ -3,8 +3,20 @@ const router = express.Router();
 
 const connection = require('../utils/db');
 
-router.get('/', (req, res, next) => {
-    res.send('hello');
-});
+router.route('/')
+    .get(async (req, res, next) => {
+        if (req.query.orderBy === 'date') {
+            let result = await connection.queryAsync('SELECT * FROM video_file WHERE valid=1 ORDER BY upload_date DESC');
+            res.json(result);
+        } else if (req.query.orderBy === 'views') {
+            let result = await connection.queryAsync('SELECT * FROM video_file WHERE valid=1 ORDER BY views DESC');
+            res.json(result);
+        } else {
+            res.json({ message: 'Not Found' });
+        }
+
+    });
+
+router.get('/');
 
 module.exports = router;
