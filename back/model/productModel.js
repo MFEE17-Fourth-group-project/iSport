f88ccd3ce73a0446c860398f2dcb6a128ee7bb73
Reverie <h221391ba@gmail.com>
2021-09-18 00:51:40 +0800

    fea(back-product):add tatol_sale to productList API

--- back/model/productModel.js
+++ back/model/productModel.js
@@ -1,15 +1,21 @@
 const connection = require('../utils/db');
 
 //all products query
-async function getAllProduct(){
+const getAllProduct = async () => {
     return await connection.queryAsync(
-        `SELECT product.id AS prduct_id, product.name AS product_name, brand.id AS brand_id, brand.name AS brand_name, product_category.id AS product_category_id, product_category.name AS product_category_name, product.intro AS product_intro, product.creat_time AS create_time, product.price AS product_price
+        `SELECT product.id AS prduct_id, product.name AS product_name, brand.id AS brand_id, 
+                brand.name AS brand_name, product_category.id AS product_category_id, 
+                product_category.name AS product_category_name, product.creat_time AS create_time, 
+                product.price AS product_price, sum(product_sku.sale) AS total_sale
         FROM product 
         INNER JOIN brand 
         ON product.brand=brand.id
         INNER JOIN product_category 
         ON product.category=product_category.id
+        INNER JOIN product_sku
+        ON product.id=product_sku.product_id
         WHERE product.valid=1
+        GROUP BY product.id
         `
     );
 }
@@ -32,3 +38,8 @@ module.exports={
 
 // 'SELECT * FROM product INNER JOIN brand on product.brand=brand.id'
 // 'SELECT brand.name AS brand_name, brand, product.id AS prduct_id FROM product INNER JOIN brand on product.brand=brand.id' 
+
+// select 單別aa,單號bb,sum(生產數量dd) as 生產數量加總,sum(報廢數量ee) as 報廢數量加總
+// from yourTable
+// where 單別aa='123' and 單號bb='456'
+// group by 單別aa,單號bb
