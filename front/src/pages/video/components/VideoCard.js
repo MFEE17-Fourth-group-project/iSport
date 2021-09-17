e9250befa093c049a825b61f2e648d530d0820a9
TaylorLiu11 <liuayo1111@gmail.com>
2021-09-17 20:40:48 +0800

    Put video details into VideoCard.js

--- front/src/pages/video/components/VideoCard.js
+++ front/src/pages/video/components/VideoCard.js
@@ -7,28 +7,28 @@ import { Link } from 'react-router-dom';
 const VideoCard = ({ video }) => {
     return (
         <Link to={'/video/' + video.id} className="max-w-max m-auto">
-            <div className="max-w-xs min-w-full rounded-md overflow-hidden shadow-2xl bg-gray-900
+            <div className="max-w-xs min-w-full h-100 rounded-md overflow-hidden shadow-2xl bg-gray-900
                 py-4 px-5 flex flex-col">
                 <img className="max-h-44 object-cover" src={cardImg1} alt="" />
                 <div className="">
-                    <h3 className="text-base text-yellow-400 my-4">{video.title}</h3>
-                    <p className="text-white text-sm text-opacity-70 leading-relaxed mb-6 overflow-ellipsis
-                        line-clamp overflow-hidden h-16">
+                    <h3 className="text-base text-yellow-400 my-4 h-12 line-clamp-2 overflow-hidden">{video.title}</h3>
+                    <p className="text-white text-sm text-opacity-70 leading-relaxed mb-6
+                        line-clamp-3 overflow-hidden h-16">
                         {video.description}
                     </p>
                 </div>
                 <div className="flex justify-between">
                     <h6 className="flex items-center">
                         <FaClock className="text-yellow-400 mr-1.5" />
-                        <span className="text-xs text-white text-opacity-85">1530</span>
+                        <span className="text-xs text-white text-opacity-85">{video.duration}</span>
                     </h6>
                     <h6 className="flex items-center">
                         <FaEye className="text-yellow-400 mr-1.5" />
-                        <span className="text-xs text-white text-opacity-85">1530</span>
+                        <span className="text-xs text-white text-opacity-85">{video.views}</span>
                     </h6>
                     <h6 className="flex items-center">
                         <FaThumbsUp className="text-yellow-400 mr-1.5" />
-                        <span className="text-xs text-white text-opacity-85">542</span>
+                        <span className="text-xs text-white text-opacity-85">{video.likes}</span>
                     </h6>
                 </div>
             </div>
