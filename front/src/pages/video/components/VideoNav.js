60dc9c6f6646b6ed0ed9c7be8b1900640ce4327e
TaylorLiu11 <liuayo1111@gmail.com>
2021-09-19 17:40:30 +0800

    Add key to navbar

--- front/src/pages/video/components/VideoNav.js
+++ front/src/pages/video/components/VideoNav.js
@@ -12,9 +12,12 @@ const VideoNav = () => {
             </div>
             {categories && (
                 categories.map(category => (
-                    <div className="border-b-2 border-transparent w-36 text-white text-center
+                    <div
+                        className="border-b-2 border-transparent w-36 text-white text-center
                         text-sm sm:text-base py-5 2xs:px-0 px-2.5 min-w-min cursor-pointer
-                        hover:border-yellow-400 hover:text-yellow-400">
+                        hover:border-yellow-400 hover:text-yellow-400"
+                        key={category.id}
+                    >
                         {category.name}
                     </div>
                 ))
