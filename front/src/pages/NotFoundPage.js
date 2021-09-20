b9ba857007bdad3cb064b03213723dd472f00387
angela1393aa <angela1393aa@gmail.com>
2021-09-20 16:03:53 +0800

    找不到頁面404

--- front/src/pages/NotFoundPage.js
+++ front/src/pages/NotFoundPage.js
@@ -3,7 +3,9 @@ import React from 'react';
 function NotFoundPage() {
     return (
         <>
-            <h1>找不到頁面 404</h1>
+            <h1 className="text-xl xs:text-2xl sm:text-3xl md:text-4xl xl:text-5xl text-white text-center h-124">
+                找不到頁面 404
+            </h1>
         </>
     );
 }
