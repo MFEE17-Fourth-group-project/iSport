8ed2257bc3727ee56244dc3c8e960df826f82b69
Sarah <hsue1207@gmail.com>
2021-09-21 16:45:28 +0800

    add cart and order api

--- back/routers/cart.js
+++ back/routers/cart.js
@@ -3,15 +3,16 @@ const router = express.Router();
 
 const connection = require("../utils/db");
 
-// api/cart 取得 database user_order 的資料
+// api/cart 取得 database product_sku jon product join brand 的資料
 router.get("/", async (req, res, next) => {
     try {
         let result = await connection.queryAsync(`
-            SELECT *, order_status.status
-            FROM user_order
-            LEFT JOIN order_status
-            ON user_order.order_status=order_status.id
+            SELECT product_sku.id AS product_sku_id, sku_code, sku_group, product_sku.price AS product_sku_price, product.name AS product_name, brand.name AS brand_name
+            FROM product
+            LEFT JOIN product_sku ON product.id=product_sku.product_id
+            LEFT JOIN brand ON product.brand=brand.id
         `);
+
         res.json(result);
     } catch (e) {
         console.error(e);
--- back/routers/order.js
+++ back/routers/order.js
@@ -6,13 +6,45 @@ const connection = require("../utils/db");
 // /api/order 取得 database user_order 的資料
 router.get("/", async (req, res, next) => {
     try {
-        let result = await connection.queryAsync(`
-            SELECT *, order_status.status, product_id
-            FROM user_order 
-            LEFT JOIN order_status ON user_order.order_status=order_status.id
-            LEFT JOIN user_order_detail ON user_order.id=user_order_detail.order_id
-        `);
-        res.json(result);
+        // 撈主訂單
+        let orders = await connection.queryAsync(`
+            SELECT * FROM user_order
+            WHERE user_id = ?;`,
+            [2]
+        );
+
+        let orderIds = orders.map((order) => {
+            return order.id;
+        });
+        console.log(orderIds);
+
+        // 撈 user_order_detail
+        let details = await connection.queryAsync(`
+            SELECT * FROM user_order_detail
+            WHERE order_id IN ?;`,
+            [[orderIds]]
+        );
+
+        // 合併主訂單與 user_order_detail
+        details.map((detail) => {
+            let order = orders.find((order) => {
+                return order.id == detail.order_id;
+            });
+            if (!order.detail) {
+                order.detail = [];
+            }
+            order.detail.push(detail);
+            // console.log(detail, order);
+        });
+
+        // let result = await connection.queryAsync(`
+        //     SELECT *, order_status.status, product_id
+        //     FROM user_order
+        //     LEFT JOIN order_status ON user_order.order_status=order_status.id
+        //     LEFT JOIN user_order_detail ON user_order.id=user_order_detail.order_id
+        // `);
+        // res.json(result);
+        res.json(orders);
     } catch (e) {
         console.error(e);
     }
