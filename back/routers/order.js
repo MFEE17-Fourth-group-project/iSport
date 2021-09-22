7524e9ebe4f7b9822eb9d6de37364fdd6b5a0492
Sarah <hsue1207@gmail.com>
2021-09-22 11:53:55 +0800

    combine table data

--- back/routers/order.js
+++ back/routers/order.js
@@ -6,44 +6,36 @@ const connection = require("../utils/db");
 // /api/order 取得 database user_order 的資料
 router.get("/", async (req, res, next) => {
     try {
-        // 撈主訂單
-        let orders = await connection.queryAsync(`
+        // 撈出(主訂單) user_order 中 user_id 為 [2] 的訂單
+        let orders = await connection.queryAsync(
+            `
             SELECT * FROM user_order
             WHERE user_id = ?;`,
             [2]
         );
+        console.log("orders", orders);
 
         let orderIds = orders.map((order) => {
             return order.id;
         });
-        console.log(orderIds);
+        console.log(orderIds); // orderIds: [ 10, 20 ]
 
-        // 撈 user_order_detail
-        let details = await connection.queryAsync(`
+        // 撈出 user_order_detail 中 order_id 為 [ 10, 20 ] 的訂單
+        let details = await connection.queryAsync(
+            `
             SELECT * FROM user_order_detail
             WHERE order_id IN ?;`,
             [[orderIds]]
         );
 
-        // 合併主訂單與 user_order_detail
-        details.map((detail) => {
-            let order = orders.find((order) => {
-                return order.id == detail.order_id;
+        // 合併 user_order 與 user_order_detail 撈出來的資料
+        orders.map((order) => {
+            // 如果 order 沒有 detail 屬性，就給他一個 []
+            order.detail = details.filter((detail) => {
+                return order.id === detail.order_id;
             });
-            if (!order.detail) {
-                order.detail = [];
-            }
-            order.detail.push(detail);
-            // console.log(detail, order);
         });
 
-        // let result = await connection.queryAsync(`
-        //     SELECT *, order_status.status, product_id
-        //     FROM user_order
-        //     LEFT JOIN order_status ON user_order.order_status=order_status.id
-        //     LEFT JOIN user_order_detail ON user_order.id=user_order_detail.order_id
-        // `);
-        // res.json(result);
         res.json(orders);
     } catch (e) {
         console.error(e);
