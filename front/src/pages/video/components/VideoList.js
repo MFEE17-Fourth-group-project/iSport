257c3dcf4ef8df6b896642323a0c830902a54fb6
TaylorLiu11 <liuayo1111@gmail.com>
2021-09-24 02:18:40 +0800

    Pagination UI

--- front/src/pages/video/components/VideoList.js
+++ front/src/pages/video/components/VideoList.js
@@ -1,11 +1,29 @@
 import VideoCard from './VideoCard';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { BiChevronLeft, BiChevronRight } from 'react-icons/bi';
 
 const VideoList = ({ videos, cat }) => {
     const [videoData, setVideoData] = useState(videos);
     const [term, setTerm] = useState('');
+    const [totalPages, setTotalPages] = useState(1);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    useEffect(() => {
+        if (parseInt(cat) !== 0) {
+            let newVideos = videos.filter(video => video.category === parseInt(cat));
+            setVideoData(newVideos);
+        } else {
+            setVideoData(videos);
+        }
+
+    }, [cat]);
+
+    useEffect(() => {
+        let total = Math.ceil(videoData.length / 8);
+        setTotalPages(total);
+        if (total === 1) setCurrentPage(1);
+    }, [videoData]);
 
     const handleUpdateButton = () => {
         let newVideos = videoData.sort((a, b) =>
@@ -28,6 +46,27 @@ const VideoList = ({ videos, cat }) => {
     const handleEmpty = (e) => {
         if (e.target.value === '') setVideoData(videos);
     };
+
+    const prevPage = () => {
+        if (currentPage > 1) setCurrentPage(currentPage - 1);
+    };
+
+    const nextPage = () => {
+        if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+    };
+
+    const changePage = (e) => {
+        setCurrentPage(parseInt(e.target.innerHTML));
+    };
+
+    const currentPageClass =
+        'first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative text-white bg-gray-900';
+
+    const notCurrentPageClass =
+        'first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative text-white bg-gray-700 hover:bg-gray-900';
+
+    const prevNextPageClass =
+        'first:ml-0 text-lg font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative text-white bg-gray-700 hover:bg-gray-900';
 
     return (
         <>
@@ -69,48 +108,37 @@ const VideoList = ({ videos, cat }) => {
             {/* VideoList */}
             <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 
                     lg:grid-cols-4 gap-4 xs:gap-2.5 sm:gap-4 pb-5">
-                {videoData && (parseInt(cat) === 0) ? videoData.map(video => (
-                    <VideoCard video={video} key={video.id} />
-                )) : videoData.filter(video => video.category === parseInt(cat)).map(video => (
+                {videoData && videoData.map(video => (
                     <VideoCard video={video} key={video.id} />
                 ))}
             </div>
 
             {/* Pagination */}
-            <nav className="flex mx-auto pb-5">
-                <ul className="flex pl-0 rounded list-none flex-wrap">
-                    <li>
-                        <button
-                            className="first:ml-0 text-lg font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative text-white bg-gray-700">
-                            <BiChevronLeft />
-                        </button>
-                    </li>
-                    <li>
-                        <button
-                            className="first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative text-white bg-gray-900">
-                            1
-                        </button>
-                    </li>
-                    <li>
-                        <button
-                            className="first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative text-white bg-gray-700">
-                            2
-                        </button>
-                    </li>
-                    <li>
-                        <button
-                            className="first:ml-0 text-xs font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative text-white bg-gray-700">
-                            3
-                        </button>
-                    </li>
-                    <li>
-                        <button
-                            className="first:ml-0 text-lg font-semibold flex w-8 h-8 mx-1 p-0 rounded-full items-center justify-center leading-tight relative text-white bg-gray-700">
-                            <BiChevronRight />
-                        </button>
-                    </li>
-                </ul>
-            </nav>
+            <div className="flex mx-auto pb-5">
+                {totalPages !== 1 &&
+                    <button
+                        className={prevNextPageClass}
+                        onClick={prevPage}
+                    >
+                        <BiChevronLeft />
+                    </button>}
+                {[...Array(totalPages).keys()].map(n => (
+                    <button
+                        className={currentPage === n + 1 ? currentPageClass : notCurrentPageClass}
+                        key={n + 1}
+                        onClick={e => changePage(e)}
+                    >
+                        {n + 1}
+                    </button>
+                ))}
+                {totalPages !== 1 &&
+                    <button
+                        className={prevNextPageClass}
+                        onClick={nextPage}
+                    >
+                        <BiChevronRight />
+                    </button>}
+            </div>
         </>
     );
 };
