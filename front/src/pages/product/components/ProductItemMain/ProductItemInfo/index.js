9107f8cad1ba9caa36a9b3293caa533288b109a0
Reverie <h221391ba@gmail.com>
2021-09-25 04:29:36 +0800

    fea(front_product):product qty change

--- front/src/pages/product/components/ProductItemMain/ProductItemInfo/index.js
+++ front/src/pages/product/components/ProductItemMain/ProductItemInfo/index.js
@@ -5,7 +5,10 @@ import ProductType from './ProductType';
 import { FaPlus, FaMinus, FaAngleLeft, FaHeart, FaShare } from 'react-icons/fa';
 
 function ProductItemInfo(props) {
+    const priceTest = 350;
+    const stockTest = 12;
     const { productInfo, typeValue } = props;
+    const [qty, setQty] = useState(1);
     const [info, setInfo] = useState({
         category: '',
         brand: '',
@@ -77,24 +80,36 @@ function ProductItemInfo(props) {
                             <span className="text-gray-400">10011031</span>
                         </div>
                         <div>
-                            庫存：<span>134</span>
+                            庫存：<span>{stockTest}</span>
                         </div>
                     </div>
                     <div className="my-2 py-2 border-b-2 border-yellow-400 flex justify-between">
                         <div className="text-yellow-400 text-lg">
-                            NT$ <span>350</span>
+                            NT$ <span>{priceTest}</span>
                         </div>
                         <div className="flex py-1 rounded-md justify-between items-center shadow-emboss">
-                            <button type="button" className="px-2">
-                                <FaPlus />
+                            <button
+                                onClick={() => {
+                                    if (qty < stockTest) setQty(qty + 1);
+                                }}
+                                type="button"
+                                className="px-2"
+                            >
+                                <FaPlus className="hover:text-yellow-400" />
                             </button>
                             <input
                                 type="text"
                                 className=" w-20 xl:w-auto bg-transparent outline-none text-center"
-                                value={1}
+                                value={qty}
                             />
-                            <button type="button" className="px-2">
-                                <FaMinus />
+                            <button
+                                onClick={() => {
+                                    if (qty > 1) setQty(qty - 1);
+                                }}
+                                type="button"
+                                className="px-2"
+                            >
+                                <FaMinus className="hover:text-yellow-400" />
                             </button>
                         </div>
                     </div>
