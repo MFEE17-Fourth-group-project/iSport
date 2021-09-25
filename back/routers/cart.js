67ab1b9dae94d98081004a4c5622920046a28510
Sarah <hsue1207@gmail.com>
2021-09-25 09:52:11 +0800

    刪除不必要中間件, cart加上登入中間件

--- back/routers/cart.js
+++ back/routers/cart.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
 const connection = require("../utils/db");
+const { SignInCheckMiddleware } = require("../middlewares/auth");
 
 // api/cart 取得 database product_sku jon product join brand 的資料
-router.get("/", async (req, res, next) => {
+router.get("/", SignInCheckMiddleware, async (req, res, next) => {
     try {
         let result = await connection.queryAsync(`
             SELECT product_sku.id AS product_sku_id, sku_code, sku_group, product_sku.price AS product_sku_price, product.name AS product_name, brand.name AS brand_name
