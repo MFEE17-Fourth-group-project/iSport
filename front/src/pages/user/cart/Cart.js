ff60b2f6cc8974b8f8b4a48d040201a09186df90
Sarah <hsue1207@gmail.com>
2021-09-26 21:08:51 +0800

    set authorization

--- front/src/pages/user/cart/Cart.js
+++ front/src/pages/user/cart/Cart.js
@@ -3,8 +3,11 @@ import { Link } from 'react-router-dom';
 import CartItem from './components/CartItem';
 import { FaArrowRight } from 'react-icons/fa';
 import Aside from '../../../global/Aside';
+import { useAuth } from '../../../context/auth';
+import NotAuth from '../components/NotAuth';
 
 function Cart(props) {
+    const { member, setMember } = useAuth();
     const showNoItemRef = useRef('');
     const hideItemRef = useRef('');
 
@@ -55,41 +58,47 @@ function Cart(props) {
 
     return (
         <>
-            <main className="sm:max-w-screen-xl w-full mx-auto px-2.5 py-5 flex lg:justify-start border-red-300">
-                <Aside />
-                <article className="w-full flex-grow flex-col">
-                    <div className="bg-gray-700 pl-5 py-5 text-white text-opacity-85 user-page-title rounded-t-xl">
-                        購物車
-                    </div>
-                    <div
-                        ref={showNoItemRef}
-                        className="h-screen text-white text-opacity-85 text-xl text-center pt-14 hidden bg-gray-900 rounded-b-xl"
-                    >
-                        您的購物車目前沒有商品！！
-                    </div>
-                    <div
-                        ref={hideItemRef}
-                        className="text-white lg:px-10 px-4 py-6 bg-gray-900 rounded-b-xl"
-                    >
-                        {/* 購物車商品卡片 */}
-                        <CartItem />
+            {member ? (
+                <main className="sm:max-w-screen-xl w-full mx-auto px-2.5 py-5 flex lg:justify-start border-red-300">
+                    <Aside />
+                    <article className="w-full flex-grow flex-col">
+                        <div className="bg-gray-700 pl-5 py-5 text-white text-opacity-85 user-page-title rounded-t-xl">
+                            購物車
+                        </div>
+                        <div
+                            ref={showNoItemRef}
+                            className="h-screen text-white text-opacity-85 text-xl text-center pt-14 hidden bg-gray-900 rounded-b-xl"
+                        >
+                            您的購物車目前沒有商品！！
+                        </div>
+                        <div
+                            ref={hideItemRef}
+                            className="text-white lg:px-10 px-4 py-6 bg-gray-900 rounded-b-xl"
+                        >
+                            {/* 購物車商品卡片 */}
+                            <CartItem />
 
-                        <div className="pt-2.5 mt-2.5 mb-9 border-t-2 border-yellow-400 text-yellow-400 flex flex-row justify-end">
-                            {/* <p className="text-lg font-bold">Total :</p>
+                            <div className="pt-2.5 mt-2.5 mb-9 border-t-2 border-yellow-400 text-yellow-400 flex flex-row justify-end">
+                                {/* <p className="text-lg font-bold">Total :</p>
                             <span className="text-lg font-bold">1400</span> */}
+                            </div>
+                            <div className="flex flex-row justify-end">
+                                <Link
+                                    to="/checkout"
+                                    className="btn-yellow flex flex-row justify-center items-center"
+                                >
+                                    <FaArrowRight className="mr-2.5" />
+                                    <p className="font-bold text-xl">
+                                        前往結帳
+                                    </p>
+                                </Link>
+                            </div>
                         </div>
-                        <div className="flex flex-row justify-end">
-                            <Link
-                                to="/checkout"
-                                className="btn-yellow flex flex-row justify-center items-center"
-                            >
-                                <FaArrowRight className="mr-2.5" />
-                                <p className="font-bold text-xl">前往結帳</p>
-                            </Link>
-                        </div>
-                    </div>
-                </article>
-            </main>
+                    </article>
+                </main>
+            ) : (
+                <NotAuth />
+            )}
         </>
     );
 }
--- front/src/pages/user/cart/TradingRecord.js
+++ front/src/pages/user/cart/TradingRecord.js
@@ -4,12 +4,15 @@ import { FaSearch } from 'react-icons/fa';
 import Aside from '../../../global/Aside';
 import OrderRecord from './components/OrderRecord';
 import axios from 'axios';
+import { useAuth } from '../../../context/auth';
+import NotAuth from '../components/NotAuth';
 
 // TODO: 依照使用者 id 拋轉他的歷史訂單
 
 function TradingRecord() {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
+    const { member, setMember } = useAuth();
     // 連到後端的 API，取得訂單記錄
     useEffect(() => {
         console.log('read API_URL', API_URL);
@@ -29,58 +32,67 @@ function TradingRecord() {
     }, []);
 
     return (
-        <main className="max-w-screen-xl mx-auto px-2.5 py-5 flex justify-start border-red-300">
-            <Aside />
-            <article className="flex-grow flex-col rounded-lg overflow-hidden text-white text-opacity-85 shadow-2xl ">
-                <div className="bg-gray-700 pl-5 py-5 user-page-title font-bold">
-                    購買紀錄
-                </div>
-                <section className="my-2.5 p-4 leading-relaxed border-2 border-yellow-400">
-                    <h6 className="text-xl ">注意事項：</h6>
-                    <p>
-                        1.產品均享有10天猶豫期之權益(注意！猶豫期非試用期)，若退/換回產品非全新狀態且包裝完整，將影響您的退貨權益及需負擔回復原狀責任。
-                        <br />
-                        2.個人衛生之商品暫不提供退/換貨，其它商品僅限換貨1次。
-                        <br />
-                        3.若無法線上操作，請與我們聯絡。
-                    </p>
-                    <h6 className="text-xl ">退/換貨規範：</h6>
-                    <p>
-                        1.取消訂單，若有多個商品於同張訂單時，須同單其他商品一同取消、退貨。
-                        <br />
-                        2.限定配送狀態為『已送達』，且為同一品號之商品，方可進行換貨服務。
-                    </p>
-                </section>
-                {/* 選取時間 */}
-                <div className="my-4 mx-auto flex items-center justify-between border-b-2 border-yellow-400 w-96">
-                    <input
-                        type="date"
-                        className="outline-none bg-transparent"
-                    />
-                    <span>至</span>
-                    <input
-                        type="date"
-                        className="outline-none bg-transparent"
-                    />
-                    <FaSearch className=" text-xl" />
-                </div>
-                <section className="text-white bg-gray-900 w-full h-full object-cover object-center text-opacity-85 text-lg lg:px-10 px-4 py-6">
-                    {/* 購買紀錄卡片 */}
-                    {data &&
-                        data.map((item, index) => (
-                            <OrderRecord
-                                key={item.id}
-                                order_no={item.order_no}
-                                order_date={item.order_date.slice(0, 10)}
-                                price={item.price}
-                                paytype={item.paytype}
-                                delivery={item.delivery}
-                                status={item.status}
+        <>
+            {member ? (
+                <main className="max-w-screen-xl mx-auto px-2.5 py-5 flex justify-start border-red-300">
+                    <Aside />
+                    <article className="flex-grow flex-col rounded-lg overflow-hidden text-white text-opacity-85 shadow-2xl ">
+                        <div className="bg-gray-700 pl-5 py-5 user-page-title font-bold">
+                            購買紀錄
+                        </div>
+                        <section className="my-2.5 p-4 leading-relaxed border-2 border-yellow-400">
+                            <h6 className="text-xl ">注意事項：</h6>
+                            <p>
+                                1.產品均享有10天猶豫期之權益(注意！猶豫期非試用期)，若退/換回產品非全新狀態且包裝完整，將影響您的退貨權益及需負擔回復原狀責任。
+                                <br />
+                                2.個人衛生之商品暫不提供退/換貨，其它商品僅限換貨1次。
+                                <br />
+                                3.若無法線上操作，請與我們聯絡。
+                            </p>
+                            <h6 className="text-xl ">退/換貨規範：</h6>
+                            <p>
+                                1.取消訂單，若有多個商品於同張訂單時，須同單其他商品一同取消、退貨。
+                                <br />
+                                2.限定配送狀態為『已送達』，且為同一品號之商品，方可進行換貨服務。
+                            </p>
+                        </section>
+                        {/* 選取時間 */}
+                        <div className="my-4 mx-auto flex items-center justify-between border-b-2 border-yellow-400 w-96">
+                            <input
+                                type="date"
+                                className="outline-none bg-transparent"
                             />
-                        ))}
-                </section>
-            </article>
-        </main>
+                            <span>至</span>
+                            <input
+                                type="date"
+                                className="outline-none bg-transparent"
+                            />
+                            <FaSearch className=" text-xl" />
+                        </div>
+                        <section className="text-white bg-gray-900 w-full h-full object-cover object-center text-opacity-85 text-lg lg:px-10 px-4 py-6">
+                            {/* 購買紀錄卡片 */}
+                            {data &&
+                                data.map((item, index) => (
+                                    <OrderRecord
+                                        key={item.id}
+                                        order_no={item.order_no}
+                                        order_date={item.order_date.slice(
+                                            0,
+                                            10
+                                        )}
+                                        price={item.price}
+                                        paytype={item.paytype}
+                                        delivery={item.delivery}
+                                        status={item.status}
+                                    />
+                                ))}
+                        </section>
+                    </article>
+                </main>
+            ) : (
+                <NotAuth />
+            )}
+        </>
     );
 }
 
