5a7ae3ff1d1d8af694a30ded8d12fbec9a3377ee
Sarah <hsue1207@gmail.com>
2021-09-26 17:51:06 +0800

    feat(front): 接 api

--- front/src/pages/user/cart/components/CheckItem.js
+++ front/src/pages/user/cart/components/CheckItem.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 function CheckItem() {
     // 取得後端資料
-    // const [data, setData] = useState([]);
+    const [qty, setQty] = useState(1);
     const [error, setError] = useState('');
     // 連到後端的 API，取得商品資訊
     console.log('read API_URL', API_URL);
@@ -19,11 +19,13 @@ function CheckItem() {
             let myCartItem = JSON.parse(localStorage.getItem('cart'));
             let productId = myCartItem.map((item) => item.product_id);
             let skuCode = myCartItem.map((item) => item.sku_code);
+            let qty = myCartItem.map((item) => item.qty);
             // console.log('myCartItem', ...myCartItem);
             // console.log('getSkuCode', skuCode);
             let result = await axios.post(`${API_URL}/cart`, {
                 productId,
                 skuCode,
+                qty,
             });
             // console.log('result.data', result.data);
             // 設定回 useState render 到網頁
@@ -33,6 +35,7 @@ function CheckItem() {
             // product_name: "【MIZUNO 美津濃】女款路跑背心 J2TA1201XX（任選）(T恤)"
             // product_sku_id: 5
             // sku_code: '10011015';
+            setQty();
             setMyCart(result.data.cartItem);
             console.log('myCart in CheckItem', myCartItem);
             setError('');
@@ -45,58 +48,58 @@ function CheckItem() {
     // componentDidMount 一進到此頁面，從 localStorage 得到 cart 資料
     useEffect(() => {
         getDataFromLocalStorage();
-        // getDataFromServer();
     }, []);
 
     return (
         <>
-            {myCart.map((item, index) => {
-                return (
-                    <div
-                        className="sm:p-2.5 h-180 lg:p-4 p-1.5 flex flex-row"
-                        key={item.id}
-                    >
-                        <div className="sm:w-36 w-2/5 sm:mx-5 mx-0 self-center text-center">
-                            <img
-                                className="w-full"
-                                src={product}
-                                alt="good product"
-                            ></img>
+            {myCart &&
+                myCart.map((item) => {
+                    return (
+                        <div
+                            className="sm:p-2.5 h-180 lg:p-4 p-1.5 flex flex-row"
+                            key={item.product_sku_id}
+                        >
+                            <div className="sm:w-36 w-2/5 sm:mx-5 mx-0 self-center text-center">
+                                <img
+                                    className="w-full"
+                                    src={product}
+                                    alt="{photo}"
+                                ></img>
+                            </div>
+                            <div className="flex flex-col flex-grow lg:ml-10 ml-5">
+                                <div className="flex flex-row justify-between pb-2.5">
+                                    <h3 className="sm:text-xl text-lg font-bold">
+                                        {item.product_name}
+                                    </h3>
+                                    <p className="text-yellow-400 font-bold">
+                                        ${item.price}
+                                    </p>
+                                </div>
+                                <div className="pb-1.5 text-sm text-yellow-400">
+                                    {item.brand_name}
+                                </div>
+                                <div className="flex flex-row items-center pb-1.5">
+                                    <p className="mr-2.5">顏色：</p>
+                                    <div className="px-4 text-white text-opacity-85 text-center text-sm rounded-full">
+                                        黑
+                                    </div>
+                                </div>
+                                <div className="flex flex-row items-center pb-1.5">
+                                    <p className="mr-2.5">尺寸：</p>
+                                    <div className="px-4 text-white text-opacity-85 text-center text-sm rounded-full">
+                                        M
+                                    </div>
+                                </div>
+                                <div className="flex flex-row items-center">
+                                    <p className="mr-2.5">數量：</p>
+                                    <div className="w-16 px-4 mr-2.5 border-l-2 border-transparent rounded-md flex items-center">
+                                        <p>1</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="flex flex-col flex-grow lg:ml-10 ml-5">
-                            <div className="flex flex-row justify-between pb-2.5">
-                                <h3 className="sm:text-xl text-lg font-bold">
-                                    女款路跑背心
-                                </h3>
-                                <p className="text-yellow-400 font-bold">
-                                    $350
-                                </p>
-                            </div>
-                            <div className="pb-1.5 text-sm text-yellow-400">
-                                MIZUNO 美津濃
-                            </div>
-                            <div className="flex flex-row items-center pb-1.5">
-                                <p className="mr-2.5">顏色：</p>
-                                <div className="px-4 text-white text-opacity-85 text-center text-sm rounded-full">
-                                    黑
-                                </div>
-                            </div>
-                            <div className="flex flex-row items-center pb-1.5">
-                                <p className="mr-2.5">尺寸：</p>
-                                <div className="px-4 text-white text-opacity-85 text-center text-sm rounded-full">
-                                    M
-                                </div>
-                            </div>
-                            <div className="flex flex-row items-center">
-                                <p className="mr-2.5">數量：</p>
-                                <div className="w-16 px-4 mr-2.5 border-l-2 border-transparent rounded-md flex items-center">
-                                    <p>1</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                );
-            })}
+                    );
+                })}
         </>
     );
 }
