d16501ef5dea39f48f3988cedf73ff275a44b2cb
angela1393aa <angela1393aa@gmail.com>
2021-10-04 17:25:52 +0800

    修改404樣式

--- front/src/pages/NotFoundPage.js
+++ front/src/pages/NotFoundPage.js
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import ArticleHeader from '../images/—Pngtree—404 error interface theme design_4043818.png';
 function NotFoundPage() {
     return (
         <>
-            <h1 className="text-xl xs:text-2xl sm:text-3xl md:text-4xl xl:text-5xl text-white text-center h-124">
-                找不到頁面 404
+            <img className="w-1/3 m-auto" src={ArticleHeader} alt="" />
+            <h1 className="text-5xl  text-white text-center">
+                抱歉!您所訪問的頁面不存在，請重新加載!
             </h1>
+            <h5 className="text-xl  text-white text-center">
+                <a
+                    href="https://zh.pngtree.com/freepng/404-error-interface-theme-design_4043818.html"
+                    target="_blank"
+                    title="連結替代文字"
+                    rel="noreferrer"
+                >
+                    圖片來自:pngtree.com
+                </a>
+            </h5>
         </>
     );
 }
