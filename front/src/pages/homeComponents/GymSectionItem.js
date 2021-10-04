52b923376838bfec3997d80cddea410e6eb47c43
angela1393aa <angela1393aa@gmail.com>
2021-10-04 15:06:09 +0800

    修改地圖跑板問題

--- front/src/pages/homeComponents/GymSectionItem.js
+++ front/src/pages/homeComponents/GymSectionItem.js
@@ -4,7 +4,7 @@ function GymSectionItem(props) {
     return (
         <div className="flex items-center my-2">
             <img className="h-24" src={image} alt="" />
-            <div className="text-white mx-3">
+            <div className="text-white mx-3 w-52">
                 <h5 className="text-2xl">{name}</h5>
                 <p className="">
                     {phone} <br />
